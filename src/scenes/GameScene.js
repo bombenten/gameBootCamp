ffefcdb8bf64755c89e05fa61eb31c1d9044e060
bombenten <50871432+bombenten@users.noreply.github.com>
2021-07-29 15:36:14 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,13 +1,5 @@
 import Phaser from "phaser";
 
-let jotaro;
-let jotaroA;
-let keyW;
-let keyA;
-let keyS;
-let keyD;
-
-
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
