0ab4fcc07e82e2205f209bcb51708b01317a0f32
jakarin-b <jakarin.b@mail.kmutt.ac.th>
2021-07-29 17:06:49 +0700

    ani

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
-             gravity: {y:300}
+            debug: false,
+            // gravity: {y:300}
         }
     },
     scene: [
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,29 +14,40 @@ class GameScene extends Phaser.Scene {
         });
     }
 
-    preload() {
-        this.load.image('milos', 'src/image/bigmilos.png');
-        this.load.image('ground','src/image/ground.png');
-        this.load.spritesheet('badboy','src/image/botei.png',
+     preload() {
+    //     this.load.image('milos', 'src/image/bigmilos.png');
+    //     this.load.image('ground','src/image/ground.png');
+         this.load.spritesheet('badboy','src/image/botei.png',
         { frameWidth: 66.5 , frameHeight: 164}); 
         //this.load.image('bg', 'src/image/TileSprite (2).png');
         //this.load.image('poke1','src/image/images/pokemon/poke (4).png');
     }
 
     create() {
-        jaigere = this.physics.add.image(220, 200, 'milos')
-        .setScale(3)
-        .setCollideWorldBounds(true);
-        ground = this.physics.add.image(250,600,'ground')
-        .setCollideWorldBounds(true)
-        .setScale(0.5,0.25);
-        this.physics.add.collider(jaigere,ground);
-        this.physics.add.overlap(jaigere,ground, ()=>this.add.sprite(220,300,'badboy').setScale(1.5));
+        // jaigere = this.physics.add.image(220, 200, 'milos')
+        // .setScale(3)
+        // .setCollideWorldBounds(true);
+        // ground = this.physics.add.image(250,600,'ground')
+        // .setCollideWorldBounds(true)
+        // .setScale(0.5,0.25);
+        // this.physics.add.collider(jaigere,ground);
+        // this.physics.add.overlap(jaigere,ground, ()=>this.add.sprite(220,300,'badboy').setScale(1.5));
         
 
-        //this.add.sprite(220,300,'badboy').setScale(1.5);
+        badboy = this.add.sprite(220,300,'badboy')
+        .setScale(3);
         //background = this.add.tileSprite(0, 0, 850, 1400, 'bg').setOrigin(0, 0).setScale(5);
         //poke1 = this.add.image(100,200,'poke1');
+        this.anims.create({
+            key: 'badAni',
+            frames: this.anims.generateFrameNumbers('badboy', {
+                start: 0,
+                end: 15
+            }),
+            duration: 1000,
+            framerate: 0,
+            repeat: -1
+        })
 
     }
     
@@ -44,6 +55,7 @@ class GameScene extends Phaser.Scene {
     
     update(delta, time) {
         //background.tilePositionY -= 0.5;
+        badboy.anims.play('badAni', true);
 
     }
 }
