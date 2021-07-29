5dc0696beb339823b5d6ed4dc6a52fa325874f8c
jakarin-b <jakarin.b@mail.kmutt.ac.th>
2021-07-29 16:09:01 +0700

    tilesprite

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 
 let jaigere;
 let badboy;
+let background;
 
 
 class GameScene extends Phaser.Scene {
@@ -13,20 +14,23 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         //this.load.image('milos', 'src/image/bigmilos.png');
-        this.load.spritesheet('badboy','src/image/botei.png',
-        { frameWidth: 66.5 , frameHeight: 164}); 
+       // this.load.spritesheet('badboy','src/image/botei.png',
+        //{ frameWidth: 66.5 , frameHeight: 164}); 
+        this.load.image('bg', 'src/image/TileSprite (2).png');
     }
 
     create() {
         //jaigere = this.add.image(100, 200, 'milos')
         //.setScale(3);
-        this.add.sprite(220,300,'badboy').setScale(1.5);
+        //this.add.sprite(220,300,'badboy').setScale(1.5);
+        background = this.add.tileSprite(0, 0, 850, 1400, 'bg').setOrigin(0, 0).setScale(5);
 
     }
     
     
     
     update(delta, time) {
+        background.tilePositionY -= 0.5;
 
     }
 }
