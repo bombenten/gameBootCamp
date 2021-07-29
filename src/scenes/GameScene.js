ca507a7d9b62e8d60319915ec50b3cd88048aaf8
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-07-29 19:13:52 +0700

    ทำพรี่โต

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,6 +9,7 @@ let keyW;
 let keyA;
 let keyS;
 let keyD;
+let PhiTo
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -19,11 +20,15 @@ class GameScene extends Phaser.Scene {
 
     preload() {
     //     this.load.image('milos','src/image/bigmilos.png');
-        this.load.spritesheet('badgay', 'src/image/botei.png',
-        { frameWidth: 66.5 , frameHeight: 164}) ;
+        // this.load.spritesheet('badgay', 'src/image/botei.png',
+        // { frameWidth: 66.5 , frameHeight: 164}) ;
     //    this.load.image('bg', 'src/image/TileSprite (2).png');
     //    this.load.image('mon', 'src/image/images/pokemon/poke (4).png');
     //    this.load.image('ground','src/image/ground.png');
+
+        this.load.spritesheet('phito', 'src/image/flyTo.png',
+        {frameWidth: 36 , frameHeight: 25 }) ;
+        
     }
 
     create() {
@@ -42,17 +47,17 @@ class GameScene extends Phaser.Scene {
     //         badgay = this.add.sprite(250, 380, 'badgay').setScale(3);
     //     });
 
-        badgay = this.physics.add.sprite(250, 380, 'badgay').setScale(3);
-        this.anims.create({
-            key: 'gayAni',
-            frames: this.anims.generateFrameNumbers('badgay', {
-                start: 0,
-                end: 15
-            }),
-            duration: 1000,
-            framerate: 0,
-            repeat: -1
-        })
+        // badgay = this.physics.add.sprite(250, 380, 'badgay').setScale(3);
+        // this.anims.create({
+        //     key: 'gayAni',
+        //     frames: this.anims.generateFrameNumbers('badgay', {
+        //         start: 0,
+        //         end: 15
+        //     }),
+        //     duration: 1000,
+        //     framerate: 0,
+        //     repeat: -1
+        // })
 
        //background = this.add.tileSprite(0, 0, 850, 1400, 'bg')
        //.setOrigin(0, 0)
@@ -69,12 +74,22 @@ class GameScene extends Phaser.Scene {
     // })
         
     //Player Control
-    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    // keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    // keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    // keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    // keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-    
+    PhiTo = this.physics.add.sprite(250, 380, 'phito').setScale(3);
+        this.anims.create({
+            key: 'ToAni',
+            frames: this.anims.generateFrameNumbers('phito', {
+                start: 0,
+                end: 15
+            }),
+            duration: 1000,
+            framerate: 0,
+            repeat: -1
+        })
 
     }
     
@@ -82,22 +97,25 @@ class GameScene extends Phaser.Scene {
     
     update(delta, time) {
        // background.tilePositionY -=0.5 ;
-       badgay.anims.play('gayAni', true);
+    //    badgay.anims.play('gayAni', true);
        
-    if(keyW.isDown){
-        badgay.setVelocityY(-500);
-    }else if(keyS.isDown){
-        badgay.setVelocityY(500);
-    }else{
-        badgay.setVelocityY(0);
-    }
-    if(keyA.isDown){
-        badgay.setVelocityX(-500);
-    }else if(keyD.isDown){
-        badgay.setVelocityX(500);
-    }else{
-        badgay.setVelocityX(0);
-    }
+    // if(keyW.isDown){
+    //     badgay.setVelocityY(-500);
+    // }else if(keyS.isDown){
+    //     badgay.setVelocityY(500);
+    // }else{
+    //     badgay.setVelocityY(0);
+    // }
+    // if(keyA.isDown){
+    //     badgay.setVelocityX(-500);
+    // }else if(keyD.isDown){
+    //     badgay.setVelocityX(500);
+    // }else{
+    //     badgay.setVelocityX(0);
+    // }
+
+    PhiTo.anims.play('ToAni', true);
+
     }
 }
 
