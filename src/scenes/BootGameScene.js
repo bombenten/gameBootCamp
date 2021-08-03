616b51938f4bd334e25a6e4053c530ea76e1f204
bombenten <50871432+bombenten@users.noreply.github.com>
2021-08-03 16:28:31 +0700

    Sprite

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 
 let milos;
+let badboy;
 
 class BootGameScene extends Phaser.Scene {
     constructor(test) {
@@ -10,11 +11,19 @@ class BootGameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('milos', 'src/image/bigmilos.png');
+        // this.load.image('milos', 'src/image/bigmilos.png');
+
+        this.load.spritesheet('badboy','src/image/botei.png',
+        { frameWidth: 67 , frameHeight: 163});
+
     }
 
     create() {
-        milos = this.add.image(200, 350, 'milos').setScale(3);
+        // milos = this.add.image(200, 350, 'milos').setScale(3);
+
+        badboy = this.add.sprite(220, 350, 'badboy').setScale(3);
+
+
     }
     
     update(delta, time) {
