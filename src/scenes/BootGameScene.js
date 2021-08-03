7a9897754ab0cd40ff565cfa3475e9b452ddba88
Nitipoom Suttachai <poom.niti1456@gmail.com>
2021-08-03 16:16:31 +0700

    add milos

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+let milos;
+
+
 
 class BootGameScene extends Phaser.Scene {
     constructor(test) {
@@ -9,11 +12,12 @@ class BootGameScene extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('milos', 'src/image/bigmilos.png');
     }
 
     create() {
-    
+        milos = this.add.image(200,350,'milos')
+        .setScale(3);
     }
     
     update(delta, time) {
