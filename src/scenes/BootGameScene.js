4baef697e347313635d0e236dc941960b0616102
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2021-08-03 17:56:12 +0700

    Spin

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -26,26 +26,45 @@ class BootGameScene extends Phaser.Scene {
 
     create() {
 
-        badboy = this.physics.add.sprite(220, 250, 'badboy').setScale(3);
-        badboy.setCollideWorldBounds(true);
+        badboy = this.physics.add.sprite(220, 350, 'badboy')
+        .setScale(3)
+        .setDepth(10)
+        .setSize(35,155)
+        .setOffset(15,0);
 
-        ground = this.physics.add.image(250, 600, 'ground').setScale(0.5, 0.1);
-        ground.setCollideWorldBounds(true);
 
-        this.physics.add.collider(badboy, ground, ()=>{
-            milos = this.add.image(200, 350, 'bigmilos').setScale(5);
-        });
+        // badboy.setCollideWorldBounds(true);
+
+        // ground = this.physics.add.image(250, 600, 'ground').setScale(0.5, 0.1)
+        // .setSize(500,300)
+        // .setOffset(0,170);
+        
+        // ground.setCollideWorldBounds(true);
+
+        // this.physics.add.collider(badboy, ground, ()=>{
+        //     milos = this.add.image(200, 350, 'bigmilos').setScale(5).setDepth(-1);
+        // });
 
         // tileSprite = this.add.tileSprite(0, 0, 850, 1400, 'pokemon')
         // .setOrigin(0, 0);
 
-
+        this.anims.create({
+            key: 'badboyAni',
+            frames: this.anims.generateFrameNumbers('badboy', {
+                start: 0,
+                end: 15
+            }),
+            duration: 1500,
+            framerate: 0,
+            repeat: -1
+        })
 
     }
 
     update(delta, time) {
         // tileSprite.tilePositionY -= 1;
-
+        
+        badboy.anims.play('badboyAni', true);
     }
 }
 
