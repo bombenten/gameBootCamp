b0e11017c904d3fcc14fc77504900049901cf2e4
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-08-03 16:36:30 +0700

    Update BootGameScene.js

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -10,17 +10,21 @@ class BootGameScene extends Phaser.Scene {
 
     preload() {
         //this.load.image('milos','src/image/bigmilos.png');
-        this.load.spritesheet('yeet','src/image/botei.png',
-        { frameWidth: 67 , frameHeight: 163}); 
+       // this.load.spritesheet('yeet','src/image/botei.png',
+        //{ frameWidth: 67 , frameHeight: 163}); 
+        this.load.image('tilesprite','src/image/TileSprite (2).png');
     }
 
     create() {
      //milos = this.add.image(200,350,'milos').setScale(3);
-     bird = this.add.sprite(425, 700, 'yeet').setScale(0.5);
+     //bird = this.add.sprite(220, 350, 'yeet').setScale(3);
+     tilesprite = this.add.tileSprite(0,0,850,1400,'tilesprite')
+     .setorigin(0,0)
+     .setScale(3);
     }
     
     update(delta, time) {
-
+        background.tilePositionY -= 10;
     }
 }
 
