c340681495e76ffb7e0d7359784da8e9df89fb1c
Nitipoom Suttachai <poom.niti1456@gmail.com>
2021-08-03 16:35:07 +0700

    add bg

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 
 let milos;
 let badboy;
+let bg;
 
 
 class BootGameScene extends Phaser.Scene {
@@ -13,17 +14,19 @@ class BootGameScene extends Phaser.Scene {
 
     preload() {
         // this.load.image('milos', 'src/image/bigmilos.png');
-        this.load.spritesheet('badboy','src/image/botei.png',
-        { frameWidth: 67 , frameHeight: 163}); 
+        // this.load.spritesheet('badboy','src/image/botei.png',
+        // { frameWidth: 67 , frameHeight: 163}); 
+        this.load.image('bg', 'src/image/TileSprite (2).png');
     }
 
     create() {
         // milos = this.add.image(200,350,'milos').setScale(3);
-        badboy = this.add.sprite(220, 350, 'badboy').setScale(2);
+        // badboy = this.add.sprite(220, 350, 'badboy').setScale(2);
+        bg = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
     }
     
     update(delta, time) {
-
+        bg.tilePositionY -= 1;
     }
 }
 
