c0961c9f3eee451eda3488515e718607f86edf43
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-08-03 17:43:05 +0700

    hitbox

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 let yeet;
-//let bg;
+let bg;
+let ground;
+ let milos;
 class BootGameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -9,15 +11,24 @@ class BootGameScene extends Phaser.Scene {
     }
 
     preload() {
-        //this.load.image('milos','src/image/bigmilos.png');
+        this.load.image('milos','src/image/bigmilos.png');
         this.load.spritesheet('yeet','src/image/botei.png',
         { frameWidth: 67 , frameHeight: 163}); 
-        //this.load.image('bg','src/image/TileSprite (2).png');
+        this.load.image('ground','src/image/ground.png');
+        this.load.image('bg','src/image/TileSprite (2).png');
     }
 
     create() {
      //milos = this.add.image(200,350,'milos').setScale(3);
-     yeet = this.add.sprite(220, 350, 'yeet').setScale(3);
+     yeet = this.physics.add.sprite(220, 250, 'yeet').setScale(3).setDepth(5);
+     yeet.setCollideWorldBounds(true) ;
+
+     ground = this.physics.add.image(100, 650, 'ground').setScale(1,1).setSize(500,90).setOffset(0,250);
+        ground.setCollideWorldBounds(true);
+        this.physics.add.collider(yeet,ground);
+        // this.physics.add.overlap(yeet,ground,()=>{
+        //     milos = this.add.image(200,350,'milos').setScale(3).setDepth(3);
+        // });
      //bg = this.add.tileSprite(0,0,850,1400,'bg')
      //.setOrigin(0,0)
      //.setScale(3)
