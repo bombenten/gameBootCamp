53a796f50ee6647b8bbbb8013354a018c1444767
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-08-03 16:53:58 +0700

    Update BootGameScene.js

--- src/scenes/BootGameScene.js
+++ src/scenes/BootGameScene.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
-
-
+let yeet;
+//let bg;
 class BootGameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -10,21 +10,22 @@ class BootGameScene extends Phaser.Scene {
 
     preload() {
         //this.load.image('milos','src/image/bigmilos.png');
-       // this.load.spritesheet('yeet','src/image/botei.png',
-        //{ frameWidth: 67 , frameHeight: 163}); 
-        this.load.image('tilesprite','src/image/TileSprite (2).png');
+        this.load.spritesheet('yeet','src/image/botei.png',
+        { frameWidth: 67 , frameHeight: 163}); 
+        //this.load.image('bg','src/image/TileSprite (2).png');
     }
 
     create() {
      //milos = this.add.image(200,350,'milos').setScale(3);
-     //bird = this.add.sprite(220, 350, 'yeet').setScale(3);
-     tilesprite = this.add.tileSprite(0,0,850,1400,'tilesprite')
-     .setorigin(0,0)
-     .setScale(3);
+     yeet = this.add.sprite(220, 350, 'yeet').setScale(3);
+     //bg = this.add.tileSprite(0,0,850,1400,'bg')
+     //.setOrigin(0,0)
+     //.setScale(3)
+     //;
     }
     
     update(delta, time) {
-        background.tilePositionY -= 10;
+        // bg.tilePositionY -= 1;
     }
 }
 
