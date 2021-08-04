5a396749fce9ed248cffb360cacdc252ccce2d64
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2021-08-04 15:33:01 +0700

    CreatTimeEvent

---
+++ src/scenes/TimeEvent.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+
+
+class TimeEvent extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'TimeEvent'
+        });
+    }
+
+    preload() {
+
+       }
+
+    create() {
+            
+    }
+
+    update(delta, time) {
+        
+    }
+}
+
+export default TimeEvent;
