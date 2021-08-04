8a0285f510c467566df732df3d09cbe0223490cd
bombenten <50871432+bombenten@users.noreply.github.com>
2021-08-04 17:30:43 +0700

    Update TimerEvent.js

--- src/scenes/TimerEvent.js
+++ src/scenes/TimerEvent.js
@@ -4,6 +4,10 @@ let badboy;
 let badSpawn;
 let badGroup;
 
+let bullet;
+let bulletGroup;
+let eventBullet;
+
 class TimerEvent extends Phaser.Scene {
     constructor(test) {
         super({
@@ -15,6 +19,8 @@ class TimerEvent extends Phaser.Scene {
         this.load.spritesheet('badboy','src/image/botei.png',
         { frameWidth: 67 , frameHeight: 163});
 
+        this.load.image('bullet', 'src/image/spike.png');
+
     }
 
     create() {
@@ -22,7 +28,13 @@ class TimerEvent extends Phaser.Scene {
         this.label = this.add.text(0, 0, '(x, y)', { fontFamily: '"Monospace"' })
         .setDepth(100);
         this.pointer = this.input.activePointer;
+        badboy = this.physics.add.sprite(200, 350, 'badboy')
+            .setScale(1.5)
+            .setDepth(10)
+            .setSize(35,155)
+            .setOffset(15,0);
         
+
         //Animation
         this.anims.create({
             key: 'badboyAni',
@@ -34,27 +46,59 @@ class TimerEvent extends Phaser.Scene {
             framerate: 0,
             repeat: -1
         })
-       
-        badGroup = this.physics.add.group();
+        badboy.anims.play('badboyAni', true);
 
-        //Event
-        badSpawn = this.time.addEvent({
-            delay: 1000,
+        //bullet Group
+        bulletGroup = this.physics.add.group()
+
+        //Event bullet
+        eventBullet = this.time.addEvent({
+            delay: 250,
             callback: function(){
-                badboy = this.physics.add.sprite(200, 720, 'badboy')
-                    .setScale(1.5)
-                    .setDepth(10)
-                    .setSize(35,155)
-                    .setOffset(15,0);
-                badGroup.add(badboy);
-                badGroup.setVelocityY(-300);
-                badboy.anims.play('badboyAni', true);
-            },
+                bullet = this.physics.add.image(badboy.x, badboy.y-110,'bullet')
+                .setScale(0.025)
+                .setSize(0.2);
+                bulletGroup.add(bullet);
+                bulletGroup.setVelocityY(-1200);
+                },
             callbackScope: this,
             loop: true,
-            startAt: 1000,
-            timeScale: 1
+            pause: false
+            });
+
+
+
+
+
+
+        this.input.on('pointermove', (pointer)=>{
+            badboy.x = pointer.x
+            badboy.y = pointer.y  //ตั้งให้ตำแหน่งตัวละครไปตามเมาส์ของเรา
         })
+       
+
+
+        // badGroup = this.physics.add.group();
+
+        //Event
+        // badSpawn = this.time.addEvent({
+        //     delay: Phaser.Math.Between(500, 1500) ,
+        //     callback: function(){
+        //         badboy = this.physics.add.sprite(Phaser.Math.Between(0, 450) , 720 +100, 'badboy')
+        //             .setScale(1.5)
+        //             .setDepth(10)
+        //             .setSize(35,155)
+        //             .setOffset(15,0);
+        //         badGroup.add(badboy);
+        //         // badboy.flipY = !badboy.flipY;
+        //         badboy.setVelocityY(Phaser.Math.Between(-200, -1000) );
+        //         badboy.anims.play('badboyAni', true);
+        //     },
+        //     callbackScope: this,
+        //     loop: true,
+        //     startAt: 1000,
+        //     timeScale: 1
+        // })
 
         
 
@@ -64,8 +108,22 @@ class TimerEvent extends Phaser.Scene {
         //Show X Y
         this.label.setText('(' + this.pointer.x + ', ' + this.pointer.y + ')');
 
+        //destroy badGroup when y = 350
+        // for (let i = 0; i < badGroup.getChildren().length; i++) {
+        //     if (badGroup.getChildren()[i].y < 350) {
+        //         badGroup.getChildren()[i].destroy();
+        //     }
+        // }
 
-    }
+        //destroy bulletGroup when y = 350
+        for (let i = 0; i < bulletGroup.getChildren().length; i++) {
+            if (bulletGroup.getChildren()[i].y < 150) {
+                bulletGroup.getChildren()[i].destroy();
+            }
+        }
+    } 
+
+    
 }
 
 export default TimerEvent;
