f43b0cf1b4cd1071a9cf5f2f7c3b8fdaa4393dda
bombenten <50871432+bombenten@users.noreply.github.com>
2021-08-15 18:59:22 +0700

    revert

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import 'phaser';
 import Phaser from 'phaser';
 import GameScene from './scenes/GameScene';
-<<<<<<< HEAD
-=======
 // import Menu from './scenes/Menu';
 // import Animation from './scenes/Animation';
->>>>>>> parent of 0ca3c27 (new)
 
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
@@ -23,14 +20,10 @@ const config = {
         }
     },
     scene: [
-<<<<<<< HEAD
-        GameScene,
-=======
         // Menu,
         GameScene
         // Animation
         
->>>>>>> parent of 0ca3c27 (new)
     ],
     
     
